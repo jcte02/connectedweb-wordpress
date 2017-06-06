c0ad73e537bc5b22d79798d7c622157b3c700af0
Fabio Endrizzi <fabio.endrizzibz@gmail.com>
2017-06-06 20:08:05 +0200

    Switch to production headers

--- includes/output/header.php
+++ includes/output/header.php
@@ -21,17 +21,7 @@ along with connectedweb.  If not, see <http://www.gnu.org/licenses/>.
 
 defined('ABSPATH') or die('OwO');
 
-// DEV
-header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');
-header('Content-Type: text/html; charset=utf-8');
-
-
-// PRODUCTION
-// header('Cache-Control: public, must-revalidate');
-// header('Last-Modified: ' . more_recent(get_lastpostmodified('gmt'), get_lastpostpublished('gmt')) . ' GMT');
+header('Cache-Control: public, must-revalidate');
+header('Last-Modified: ' . get_lastpostmodified('gmt') . ' GMT');
 
 require_once('encode.php');
